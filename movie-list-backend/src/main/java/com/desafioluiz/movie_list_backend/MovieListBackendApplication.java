4b2deabee85dacac15b2eea47e5352664663c198
HeDuz7 <notluiz901@gmail.com>
2024-08-30 09:27:08 -0300

    Primeiro Commit

---
+++ movie-list-backend/src/main/java/com/desafioluiz/movie_list_backend/MovieListBackendApplication.java
@@ -0,0 +1,13 @@
+package com.desafioluiz.movie_list_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieListBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieListBackendApplication.class, args);
+	}
+
+}
---
+++ movie-list-backend/src/test/java/com/desafioluiz/movie_list_backend/MovieListBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafioluiz.movie_list_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieListBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
